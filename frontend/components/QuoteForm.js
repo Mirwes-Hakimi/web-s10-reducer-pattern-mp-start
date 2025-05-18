770993bebedfd250fdaf82ab52c8ea410e518967
Mirwes Hakimi <mirwes215@gmail.com>
2025-05-18 15:48:54 -0700

    created reducer funtion for QuouteForm component

--- frontend/components/QuoteForm.js
+++ frontend/components/QuoteForm.js
@@ -5,6 +5,47 @@ import React,{ useReducer } from 'react' // 👈 you'll need the reducer hook
 const CHANGE_INPUT = 'CHANGE_INPUT'
 const RESET_FORM = 'RESET_FORM'
 
+// 👇 create your initial state object here
+const initialState = {
+  authorName: '',
+  quoteText: '',
+}
+// 👇 create your reducer function here
+const reducer = (state, action) => {
+  switch (action.type){
+    case CHANGE_INPUT:{
+      const { name, value } = action.payload
+      return { ...state, [name]: value}
+    }
+    case RESET_FORM: {
+      return { autorName: '', quoteText:''}
+    }
+    default: {
+      return state
+    }
+
+  }
+}
+
+export default function TodoForm({ createQuote }) {
+  // 👇 use the reducer hook to spin up state and dispatch
+const [state, dispatch] = useReducer(reducer, initialState)
+
+  const onChange = ({ target: { name, value } }) => {
+    // 👇 implement
+     dispatch({ type: CHANGE_INPUT, payload: { name, value }})
+  }
+  const resetForm = () => {
+    // 👇 implement
+    dispatch({ type: RESET_FORM })
+  }
+  const onNewQuote = (evt) => {
+    // 👇 implement
+    evt.preventDefault()
+    const { authorName, quoteText } = state;
+    createQuote({ authorName, quoteText })
+    resetForm()
+  }
 
   // 👇 some props are missing in the JSX below:
   return (
